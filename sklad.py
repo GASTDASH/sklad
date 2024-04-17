26e6d6d6951d75906b22f497313316295ca84a73
GASTDASH <gastdash@gmail.com>
2024-04-17 23:45:11 +0300

    end

--- sklad.py
+++ sklad.py
@@ -31,12 +31,12 @@ class StorageWindow(QWidget):
         self.refresh_button.clicked.connect(self.refresh)
         layout.addWidget(self.refresh_button, 0, 0)
 
-        # Кнопка "Сохранить"
-        self.save_button = QPushButton(self)
-        self.save_button.setStyleSheet("font-size: 14px; background-color: #66de70; color: white;")
-        self.save_button.setText("Сохранить")
-        self.save_button.clicked.connect(self.save)
-        layout.addWidget(self.save_button, 2, 0)
+        # # Кнопка "Сохранить"
+        # self.save_button = QPushButton(self)
+        # self.save_button.setStyleSheet("font-size: 14px; background-color: #66de70; color: white;")
+        # self.save_button.setText("Сохранить")
+        # self.save_button.clicked.connect(self.save)
+        # layout.addWidget(self.save_button, 2, 0)
 
         # Кнопка "Добавить"
         self.add_button = QPushButton(self)
@@ -45,12 +45,19 @@ class StorageWindow(QWidget):
         self.add_button.clicked.connect(self.add)
         layout.addWidget(self.add_button, 0, 1)
 
+        # Кнопка "Изменить"
+        self.edit_button = QPushButton(self)
+        self.edit_button.setStyleSheet("font-size: 14px; background-color: #43a4e6; color: white;")
+        self.edit_button.setText("Изменить")
+        self.edit_button.clicked.connect(self.edit)
+        layout.addWidget(self.edit_button, 1, 1)
+
         # Кнопка "Удалить"
         self.remove_button = QPushButton(self)
         self.remove_button.setStyleSheet("font-size: 14px; background-color: #cf392b; color: white;")
         self.remove_button.setText("Удалить")
         self.remove_button.clicked.connect(self.remove)
-        layout.addWidget(self.remove_button, 1, 1)
+        layout.addWidget(self.remove_button, 2, 1)
 
         # Таблица базы данных
         self.table = QTableWidget(self)
@@ -107,13 +114,18 @@ class StorageWindow(QWidget):
         else:
             print("Отмена удаления!")
 
-    def save(self):
-        pass
+    def edit(self):
+        index = self.table.currentRow()
+        dlg = EditDialog(self, index)
+        if dlg.exec():
+            print("Изменение товара выполенено!")
+        else:
+            print("Отмена изменения!")
 
 
 class AddDialog(QDialog):
-    def __init__(self, parent=None):
-        super().__init__(parent)
+    def __init__(self):
+        super().__init__()
 
         self.setWindowTitle("Добавление товара")
 
@@ -125,6 +137,92 @@ class AddDialog(QDialog):
         self.buttonBox.rejected.connect(self.cancel_click)
 
         self.title = QLabel("Введите информацию о товаре")
+        self.title.setStyleSheet("font-size: 16px;")
+        self.layout.addWidget(self.title)
+
+        self.name_label = QLabel("Наименование товара:")
+        self.layout.addWidget(self.name_label)
+        self.name_box = QLineEdit()
+        self.layout.addWidget(self.name_box)
+
+        self.count_label = QLabel("Текущее количество:")
+        self.layout.addWidget(self.count_label)
+        self.count_box = QLineEdit()
+        self.layout.addWidget(self.count_box)
+
+        self.type_of_count_label = QLabel("Тип количества:")
+        self.layout.addWidget(self.type_of_count_label)
+        # self.type_of_count_box = QLineEdit()
+        self.type_of_count_box = QComboBox()
+        self.type_of_count_box.addItem('шт')
+        self.type_of_count_box.addItem('кг')
+        self.layout.addWidget(self.type_of_count_box)
+
+        self.deliver_label = QLabel("Поставщик (полностью точное имя):")
+        self.layout.addWidget(self.deliver_label)
+        # self.deliver_box = QLineEdit()
+        self.deliver_box = QComboBox()
+
+        res = supabase.table("delivers").select("name").execute()
+        data = res.data
+        # delivers = list()
+        for row in data:
+            # delivers.append(row["name"])
+            self.deliver_box.addItem(row["name"])
+
+        self.layout.addWidget(self.deliver_box)
+
+        self.last_delivery_label = QLabel("Дата последней поставки (в формате ГГГГ:ММ:ДД):")
+        self.layout.addWidget(self.last_delivery_label)
+        self.last_delivery_box = QLineEdit()
+        self.layout.addWidget(self.last_delivery_box)
+
+        self.layout.addWidget(self.buttonBox)
+        self.setLayout(self.layout)
+
+    def save_click(self):
+        deliver = self.deliver_box.currentText()
+        res = supabase.table("delivers").select("deliver_id").eq("name", deliver).execute()
+        data = res.data
+        count = len(data)
+
+        if count != 0:
+            id = data[0]["deliver_id"]
+
+            supabase.table("storage").insert(
+                {
+                    "name": self.name_box.text(),
+                    "count": self.count_box.text(),
+                    "type_of_count": self.type_of_count_box.currentText(),
+                    "deliver_id": id,
+                    "last_delivery": self.last_delivery_box.text()
+                }
+            ).execute()
+
+            self.accept()
+        else:
+            print("Неправильно введён поставщик!")
+
+    
+    def cancel_click(self):
+        self.reject()
+
+class EditDialog(QDialog):
+    def __init__(self, parent, index: int):
+        super().__init__(parent)
+
+        self.index = index
+
+        self.setWindowTitle("Изменение товара")
+
+        self.layout = QVBoxLayout()
+
+        QBtn = QDialogButtonBox.Save | QDialogButtonBox.Cancel
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.save_click)
+        self.buttonBox.rejected.connect(self.cancel_click)
+
+        self.title = QLabel("Информация о товаре")
         self.title.setStyleSheet("font-size: 16px;")
         self.layout.addWidget(self.title)
 
